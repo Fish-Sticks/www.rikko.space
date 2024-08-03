2ecb22366b29b5c21bdbbd41dc113e3e896a72a6
newrlkko <166535715+rllko@users.noreply.github.com>
2024-08-03 17:35:49 +0100

    initial

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # React + Vite
 
-Live on: https://rllko.github.io/shopping-list-web-app/
-
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
 Currently, two official plugins are available:
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/',
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
-    },
-  },
+  "scripts": {
+    "build": "vite build",
+    "preview": "vite preview"
+    
+  }
 })
- 
- 
