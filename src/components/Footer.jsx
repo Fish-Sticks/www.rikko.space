800e2127fab30fa3a67d1b4bb6b493c32662dd9a
newrlkko <166535715+rllko@users.noreply.github.com>
2024-08-03 16:32:02 +0100

    a

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer className="footer bg-light text-center text-lg-start mt-auto fixed-bottom">
-            <div className="text-center p-3"><p>c 2024 Bookshelf</p></div>
-        </footer>
-    )
-}
-
-export default Footer;
