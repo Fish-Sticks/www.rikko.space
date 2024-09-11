9d79232f007afd1b7f929f8d02d2aef97cc96606
newrlkko <166535715+rllko@users.noreply.github.com>
2024-09-11 16:11:38 +0100

    yo

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+// import './assets/input.css'
+// import './assets/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+          <div style='background: black;margin:auto;max-width: 50vw;'>
+                <img src='https://http.cat/418' />
+    	</div>
   </React.StrictMode>,
 )
